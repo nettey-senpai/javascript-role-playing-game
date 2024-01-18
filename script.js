c242ebb97653534f8366e72f7cfaf8f1b65bc00f
Jeffrey <netteyjeff@gmail.com>
2024-01-18 15:06:57 +0000

    feat: add hit or miss when player attacks a monster

--- script.js
+++ script.js
@@ -205,9 +205,13 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeapon].name + ".";
-  health -= monsters[fighting].level;
-  monsterHealth -=
-    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -216,6 +220,13 @@ function attack() {
     fighting === 2 ? winGame() : defeatMonster();
   }
 }
+
+function getMonsterAttackValue(level) {
+  const hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
 function dodge() {
   text.innerText =
     "You dodge the attack from the " + monsters[fighting].name + "";
@@ -247,4 +258,8 @@ function restart() {
   healthText.innerText = health;
   xpText.innerText = xp;
   goTown();
+}
+
+function isMonsterHit() {
+  return Math.random() > 0.2 || health < 20;
 }
